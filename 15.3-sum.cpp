f59bcb2ffbcb9a67889a2320f5b4f798fdc6ca18
630445639 <630445639@qq.com>
2019-09-29 16:38:59 +0800

    update 15.3-sum

--- 15.3-sum.cpp
+++ 15.3-sum.cpp
@@ -13,62 +13,60 @@ public:
     vector<vector<int> > threeSum(vector<int>& nums) {
     sort(nums.begin(),nums.end());
     vector<vector<int> > result;
-    if (nums.size()<3)
+    if (nums.size()<3||nums.front()>0||nums.back()<0)
     {
         return result;
     }
     
     for (int i = 0; i <nums.size()-2; i++){
-        if (i>=1) {
-        if (nums[i]==nums[i-1])
-     {
-         continue;
-     }
+        if (nums[i]>0)
+        {
+            break;
         }
+        
+        if (i>0&&nums[i]==nums[i-1])
+        {
+            continue;
+        }
+        
           
         
         int j=i+1,k=nums.size()-1;
         while (j<k)
         {
-            if ((j>i+1)&&(j<nums.size()-1)&&(nums[j]==nums[j-1]))
+            if (nums[i]+nums[j]+nums[k]<0)
             {
                 j++;
-                continue;
-            }
-            if ((k>1)&&(k<nums.size()-1))
-            {
-                if (nums[k]==nums[k+1])
-                {
-                     k--;
-                continue;
-                }
                 
-               
             }
-            
-            if (nums[j]+nums[k]>-nums[i])
+            else if (nums[i]+nums[j]+nums[k]>0)
             {
                 k--;
                 
-                
-                
             }
-            else if  (nums[j]+nums[k]==-nums[i]){
-                
+           
+           else if (nums[i]+nums[j]+nums[k]==0)
+            {        
                 vector<int> temp;
                 temp.push_back(nums[i]);
                 temp.push_back(nums[j]);
                 temp.push_back(nums[k]);
+                while (j<k&&nums[j]==nums[j+1])
+                {
+                    j++;
+                }
+                while (k>j&&nums[k]==nums[k-1])
+                {
+                    k--;
+                }
                 j++;
                 k--;
+                
+                
                 result.push_back(temp);
                 
             }
-            else if (nums[j]+nums[k]<-nums[i])
-            {
-                j++;
-                
-            }
+            
     }
     }
     return result;
