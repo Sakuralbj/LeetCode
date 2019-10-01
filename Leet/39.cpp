4c3442dcb45e117d874170cbcf64ab212b6f0981
630445639 <630445639@qq.com>
2019-10-01 10:22:57 +0800

    add 39.cpp

---
+++ Leet/39.cpp
@@ -0,0 +1,36 @@
+//
+// Created by jianbo on 2019-09-30.
+//回溯
+//
+#include "iostream"
+#include "vector"
+using namespace std;
+
+class Solution {
+private:
+    vector<int> candidates;
+    vector<vector<int>> res;
+    vector<int> path;
+
+public:
+    void DFS(int start, int target) {
+        if (target == 0) {
+            res.push_back(path);
+            return;
+        }
+
+        for (int i = start; i < candidates.size() && candidates[start] <= target; ++i) {
+
+            path.push_back(candidates[i]);
+            DFS(i, target - candidates[i]);
+            path.pop_back();
+        }
+        return;
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        this->candidates=candidates;
+        DFS(0,target);
+        return res;
+    }
+};
