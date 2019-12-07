4b75b1247a1c3df8a8bca40ec833a882235180c5
Sakuralbj <630445639@qq.com>
2019-12-07 10:53:48 +0800

    add 33.cpp 34.cpp

--- Leet/33.cpp
+++ Leet/33.cpp
@@ -1,19 +1,62 @@
 //
 // Created by 马建波 on 2019-12-03.
 //
+#include "iostream"
+#include "vector"
+using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int l=0,r=nums.size()-1;
-        int mid=(l+r)/2;
-
-        if(nums[mid]<num[l]){
-            if(nums[mid]==target){
-                return mid;
-            } else if(nums[mid])
+        while (l<=r){
+            int mid=(l+r)/2;
+        if(nums[mid]==target){
+            return mid;
         }
+
+        if(nums[mid]<nums[l]){
+
+            if(target<=nums[r]&&target>nums[mid]){
+                l=mid+1;
+            } else{
+                r=mid-1;
+            }
+
+        } else if(nums[mid]>nums[r]) {//拐点在右边
+            if (target >=nums[l] && target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        } else {
+            if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+            cout<<l;
+
+        }
+        return -1;
+
 
 
 
     }
 };
+int main(){
+    Solution s1;
+    vector<int> nums;
+    nums.push_back(4);
+    nums.push_back(5);
+    nums.push_back(6);
+    nums.push_back(7);
+    nums.push_back(0);
+    nums.push_back(1);
+    nums.push_back(2);
+    cout<<s1.search(nums,0);
+
+    
+}
---
+++ Leet/34.cpp
@@ -0,0 +1,66 @@
+//
+// Created by 马建波 on 2019-12-05.
+//
+#include "vector"
+#include  "iostream"
+using namespace std;
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result={-1,-1};
+        if(nums.size()==0){
+            return result;
+        }
+        int left=0,right=nums.size()-1;
+        int l,r,mid;
+
+        while (left<=right){
+             mid=(left+right)/2;
+            if(nums[mid]==target){
+                l=mid;
+                r=mid;
+                break;
+            } else if(nums[mid]>target){
+                right=mid-1;
+            } else{
+                left=mid+1;
+            }
+        }
+        if(nums[mid]!=target){
+            return result;
+
+        }
+        while(l>=left){
+            mid=(left+l)/2;
+
+            if(nums[mid]==target){
+                if(mid==left||nums[mid-1]!=target){
+                    result[0]=mid;
+                    break;
+                } else{
+                    l=mid-1;
+                }
+
+
+            } else{
+                left=mid+1;
+            }
+        }
+        while (r<=right){
+            mid=(right+r)/2;
+            if(nums[mid]==target){
+                if(mid==right||nums[mid+1]!=target){
+                    result[1]=mid;
+                    break;
+                    } else{
+                    r=mid+1;
+                    }
+
+            } else{
+                right=mid-1;
+            }
+        }
+
+        return result;
+    }
+    };
