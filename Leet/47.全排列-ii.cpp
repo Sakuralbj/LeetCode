bda13a15f2ff2e7fd0fda795257f4eb5e401f327
Sakuralbj <sakuranlbj@gmail.com>
2020-01-31 16:10:55 +0800

    update

--- Leet/47.全排列-ii.cpp
+++ Leet/47.全排列-ii.cpp
@@ -1,17 +1,18 @@
 /*
- * @lc app=leetcode.cn id=46 lang=cpp
+ * @lc app=leetcode.cn id=47 lang=cpp
  *
- * [46] 全排列
+ * [47] 全排列 II
  */
 
 // @lc code=start
 #include<iostream>
 #include<vector>
+#include<set>
 using namespace std;
 class Solution {
 public:
     vector<vector<int>> result;
-    vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
         int len=nums.size();
         
         bool judge[100]={0};
@@ -21,14 +22,25 @@ public:
         return result;
     }
     void permutehelp(vector<int> nums,vector<int> tmp,bool judge[],int len){
+        
         if(tmp.size()==len){
             result.push_back(tmp);
             return;
 
         }
+        set<int> s;
         for (int i = 0; i < nums.size(); i++)
-        {  
+        {   
+            
+            
             if(judge[i]==0){
+                if(s.count(nums[i])==0){
+                
+                s.insert(nums[i]);
+            }else
+            {
+                continue;
+            }
                 tmp.push_back(nums[i]); 
                 judge[i]=1;
                 permutehelp(nums,tmp,judge,len);
@@ -39,13 +51,7 @@ public:
             
         }
         
-    }
-
-
-
-
-
-        
+    }   
     
 };
 // @lc code=end
