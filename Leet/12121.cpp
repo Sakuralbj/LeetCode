0e256a0d9a6f09b0049d752778dbd9e800582735
Sakuralbj <sakuranlbj@gmail.com>
2020-03-01 16:54:26 +0800

    add 204.cpp

--- Leet/12121.cpp
+++ Leet/12121.cpp
@@ -3,23 +3,23 @@
 #include<map>
 using namespace std;
 
-vector<map<char,int>>vt;
-bool cmp(char a,char b){
-    for(int i=0;i<vt.size();i++){
-    map<char,int>mp=vt[i];
-    int l=(mp.count(a)==1)?mp[a]:0;
-    int r=(mp.count(b)==1)?mp[b]:0;
-    if(l!=r){
-        return l>r;
-    }
-    }
-    return a<b;
 
-}
+// bool cmp(char a,char b){
+//     for(int i=0;i<vt.size();i++){
+//     map<char,int>mp=vt[i];
+//     int l=(mp.count(a)==1)?mp[a]:0;
+//     int r=(mp.count(b)==1)?mp[b]:0;
+//     if(l!=r){
+//         return l>r;
+//     }
+//     }
+//     return a<b;
+
+// }
 class Solution {
-    
+    vector<map<char,int>>vt;
 public:
-   
+    vector<map<char,int>>vt;
     string rankTeams(vector<string>& votes) {
         if(votes.size()==1){
             return votes[0];
@@ -32,22 +32,34 @@ public:
             }
             vt.push_back(mp);
         }
-        
-        sort(votes[0].begin(),votes[0].end(),cmp);
-        return votes[0];
-        
+        sort(votes[0].begin(), votes[0].end(), [](char a, char b) {
+            for(int i=0;i<vt.size();i++){
+            map<char,int>mp=vt[i];
+            int l=(mp.count(a)==1)?mp[a]:0;
+            int r=(mp.count(b)==1)?mp[b]:0;
+            if(l!=r){
+                return l>r;
+            }
+            }
+            return a<b;
+        }
 
-    }
-};
-int main(){
-    Solution s;
-    vector<string>votes;
-    votes.push_back("ABC");
-    votes.push_back("ACB");
-    votes.push_back("ABC");
-    votes.push_back("ACB");
-    votes.push_back("ACB");
-    votes.push_back("ACB");
+
     
-    cout<<s.rankTeams(votes);
-}
+        return votes[0];
+    }
+
+    
+};
+// int main(){
+//     Solution s;
+//     vector<string>votes;
+//     votes.push_back("BCA");
+//     votes.push_back("CAB");
+//     votes.push_back("CBA");
+//     votes.push_back("ABC");
+//     votes.push_back("ACB");
+//     votes.push_back("BAC");
+    
+//     cout<<s.rankTeams(votes);
+// }
---
+++ Leet/204.质数.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n==0){
+            return 0;
+        }
+        bool isPrime[n];
+        memset(isPrime,true,sizeof(isPrime));
+        for(int i=2;i*i<n;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<n;j+=i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+        return count;
+
+    }
+};
